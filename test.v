7466a42a91197d4de559f467fb299bd7adc8c60b
MasonKabat32 <mk964421@ohio.edu>
2024-11-07 13:57:29 -0500

    32 bit ALU and tests completed

--- test.v
+++ test.v
@@ -155,3 +155,28 @@ module ALU16bit(a, b, cin, less, op, result, cout, set, zero, g, p, overflow);
   assign result = (op[2] && result3[3] == 1) ? ~{result3, result2, result1, result0} : {result3, result2, result1, result0}; // Directly combine results
   assign cout = C4; // Connect the final carry-out
 endmodule // ALU16bit
+
+//32 bit ALU module
+module ALU32bit(a, b, op, result, set, zero, overflow);
+    input [31:0] a, b;
+    input [2:0] op; // op[2] is "binv". op[1:0] denotes the 2-bit operation.
+    output [31:0] result;
+    output set; // set is the result of the most-significant ADDER unit.
+    output zero; // zero is 1 if the result is 0x0000. Otherwise, it is 0.
+    output overflow;
+    wire cout0; //carry out from alu0
+    wire cout1; //carry out of the upper 16 bits
+    wire [15:0]result0; //Results from each 16 bit individually
+    wire [15:0]result1;
+    wire zero0; //zero output from alu0
+    wire zero1; //zero output from alu1
+    wire set1; //set from the upper 16 bit alu
+
+    ALU16bit alu0(.a(a[15:0]), .b(b[15:0]), .cin(op[2]), .less(set1), .op(op[2:0]), .result(result0), .cout(cout0), .zero(zero0));
+    ALU16bit alu1(.a(a[31:16]), .b(b[31:16]), .cin(cout0), .less(1'b0), .op(op[2:0]), .result(result1), .cout(cout1), .set(set1), .zero(zero1));
+
+    assign result = {result1, result0}; //Combine 16 bit ALU results
+    assign set = set1;                  //Calculate set
+    assign zero = zero0 & zero1;        // Calculate zero
+    assign overflow = zero & 1'b0;      //Detect if there is overflow
+endmodule
--- testBench.v
+++ testBench.v
@@ -47,3 +47,50 @@ module testBench;
         $finish;
     end
 endmodule
+
+
+module tests32;
+    reg [31:0] a,b; //input registers
+    reg [2:0]op;    //op code register
+    wire [31:0]result; //wire to capture the result for each test
+    wire set, zero, overflow; //Wires to catch other outputs of 32 bit ALU
+
+    //Instantiate 32 bit ALU
+    ALU32bit uuv(
+        .a(a),
+        .b(b),
+        .op(op),
+        .result(result),
+        .set(set),
+        .zero(zero),
+        .overflow(overflow)
+    );
+
+    //Run test cases
+    initial begin
+        $monitor("a = %b, b = %b, op = %b | result = %b, set = %b, zero = %b, overflow = %b", a, b, op, result, set, zero, overflow);
+
+        //Test Case 1: Addition
+        a = 32'b00000000000000000000000000000001; b = 32'b00000000000000000000000000000001; op = 3'b010;
+        #10;
+        $display("Addition: result = %h, set = %b, zero = %b", result, set, zero);
+
+        // Test case 2: Subtraction
+        a = 32'b00000000000000000000000000000111; b = 32'b00000000000000000000000000000011; op = 3'b110;
+        #10;
+        $display("Subtraction: result = %h, set = %b, zero = %b", result, set, zero);
+
+        // Test case 3: AND
+        a = 32'b00000000000000000000000000010111; b = 32'b00000000000000000000000000000111; op = 3'b000;
+        #10;
+        $display("AND: result = %h, set = %b, zero = %b", result, set, zero);
+
+        // Test case 4: OR
+        a = 32'b00000000000000000000000000000101; b = 32'b00000000000000000000000000001010; op = 3'b001;
+        #10;
+        $display("OR: result = %h, set = %b, zero = %b", result, set, zero);
+        
+        $finish;
+    end
+
+endmodule
